1cd7253f6a728e78b7d99a72df9b0a59be18214d
Waleed Hammam <waleed.hammam@gmail.com>
2022-11-29 11:44:56 +0200

    fix azure pipelines (#13)
    * fix azure pipelines
    
    * add azure image to pipeline
    
    * add azure image to pipeline

---
+++ azure/entrypoint_azure.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+##
+# Created entrypoint for azure devops specially as it's only supports bash in posix mode:
+# https://www.gnu.org/software/bash/manual/html_node/Bash-POSIX-Mode.html
+# in our case if is using [ ] not [[ ]] also operation like ${WEAVE_REPO_BRANCH/#$REF_PREFIX} is not supported
+##
+
+# AzureDevops
+export WEAVE_REPO_PROVIDER="azure-devops"
+export WEAVE_REPO_URL="${SYSTEM_COLLECTIONURI}_git/${BUILD_REPOSITORY_NAME}"
+export WEAVE_REPO_BRANCH="${BUILD_SOURCEBRANCHNAME}"
+export WEAVE_REPO_SHA="${BUILD_SOURCEVERSION}"
+export AZURE_PROJECT="${SYSTEM_TEAMPROJECT}"
+
+
+exec weave-iac-validator "$@"
